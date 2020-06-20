955b8c36bfd86f2adfb8e7151a21dea45829a7fc
morganamilo <morganamilo@archlinux.org>
2020-06-20 19:24:08 +0100

    Bap

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,18 @@ impl EventHandler for Handler {
     }
 
     fn message(&self, ctx: Context, message: Message) {
-        if !message.is_own(&ctx)
-            && message
-                .content
-                .replace(char::is_whitespace, "")
-                .to_lowercase()
-                .contains("bap")
-        {
+        let count = has_bap(&message.content);
+        if !message.is_own(&ctx) && count > 0 {
             if message.mention_everyone {
                 if let Some(guild) = message.guild(&ctx) {
                     let users = guild.read();
                     let users = users.members.values().map(|v| v.user.read().clone());
-                    bap(&ctx, &message, users);
+                    bap(&ctx, &message, users, count);
                 }
             } else if message.mentions.is_empty() {
-                bap(&ctx, &message, iter::once(message.author.clone()));
+                bap(&ctx, &message, iter::once(message.author.clone()), count);
             } else {
-                bap(&ctx, &message, message.mentions.iter().cloned());
+                bap(&ctx, &message, message.mentions.iter().cloned(), count);
             }
 
             react(&ctx, &message, ReactionType::Unicode("🇧".into()));
@@ -45,12 +40,26 @@ fn react(ctx: &Context, message: &Message, reaction: ReactionType) {
     }
 }
 
-fn bap(ctx: &Context, message: &Message, mentions: impl Iterator<Item = User>) {
+fn has_bap(s: &str) -> usize {
+    s.replace(char::is_whitespace, "")
+        .to_lowercase()
+        .chars()
+        .bap()
+        .collect::<String>()
+        .matches("bap")
+        .count()
+}
+
+fn bap(ctx: &Context, message: &Message, mentions: impl Iterator<Item = User>, count: usize) {
     let mut msg = String::new();
 
     for user in mentions {
         msg.push_str(&user.mention());
-        msg.push_str(": Bap\n");
+        msg.push_str(":");
+        for _ in 0..count {
+            msg.push_str(" Bap");
+        }
+        msg.push_str("\n");
     }
 
     if let Err(err) = message.channel_id.send_message(ctx, |m| m.content(msg)) {
@@ -63,5 +72,41 @@ fn main() {
 
     if let Err(err) = client.start() {
         println!("runtime error: {:?}", err);
+    }
+}
+
+struct Bap<I: Iterator> {
+    iter: I,
+    last: Option<I::Item>,
+}
+
+impl<I: Iterator> Iterator for Bap<I>
+where
+    I::Item: PartialEq + Clone,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<I::Item> {
+        while let Some(cur) = self.iter.next() {
+            if Some(&cur) != self.last.as_ref() {
+                self.last = Some(cur.clone());
+                return Some(cur);
+            }
+        }
+
+        None
+    }
+}
+
+trait Bappable<I: Iterator> {
+    fn bap(self) -> Bap<I>;
+}
+
+impl<I: Iterator> Bappable<I> for I {
+    fn bap(self) -> Bap<I> {
+        Bap {
+            iter: self,
+            last: None,
+        }
     }
 }
